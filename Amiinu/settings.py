f17b238a56c0e3094b788c024cd3bc509bbadb1c
Billal Ahmed <malikbilalahmed59>
2025-01-18 01:21:29 +0500

    new email verfication template

--- Amiinu/settings.py
+++ Amiinu/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # Directory to collect static files for deployment
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
