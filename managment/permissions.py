fb18ce30cc0400f71f5eba02a913db6822f20cb3
Malikbilalahmed59@gmail.com <malikbilalahmed59@gmail.com>
2025-04-28 07:39:51 +0500

    done notifications changes websoket

--- managment/permissions.py
+++ managment/permissions.py
@@ -3,12 +3,9 @@ from rest_framework.permissions import BasePermission
 class IsShipmentOrWarehouseOrAdmin(BasePermission):
 
     def has_permission(self, request, view):
-        # Superusers and staff always have permission
-        if request.user.is_authenticated and (request.user.is_superuser or request.user.is_staff):
-            return True
-
-        # Otherwise, check the role as usual
-        return request.user.is_authenticated and request.user.role in ['shipment', 'warehouse']
+        return request.user.is_authenticated and (
+                request.user.is_superuser or (request.user.role == "warehouse"or request.user.role == "shipment")
+        )
 
 
 
