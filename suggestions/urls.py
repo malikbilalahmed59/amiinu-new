277612f9f76e5896b0bbcb6de2ba2d708387c628
Malikbilalahmed59@gmail.com <malikbilalahmed59@gmail.com>
2025-06-28 11:30:49 +0500

    doing some changes

--- suggestions/urls.py
+++ suggestions/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ShippingRateViewSet, address_suggestions
+from .views import ShippingRateViewSet, address_suggestions, ControlRateViewSet
 
 router = DefaultRouter()
 router.register(r'shipping-rates', ShippingRateViewSet, basename='shipping-rates')
+router.register(r'control-rates', ControlRateViewSet, basename='control-rates')
+
 
 urlpatterns = [
     path('', include(router.urls)),
